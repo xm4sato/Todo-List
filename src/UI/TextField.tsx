d3acc02a4dc1aa337fb4c82afe41b9e61746182c
xm4sato <xm4sato@gmail.com>
2026-05-21 14:36:06 +0300

    refactor: make Text Field More Customizable and fixs bugs

--- src/UI/TextField.tsx
+++ src/UI/TextField.tsx
@@ -1,53 +1,49 @@
-import { TextField, type FormHelperTextProps, type TextFieldVariants } from "@mui/material";
-import { useState, type PropsWithChildren, type ProviderProps } from "react";
-import type { errorText } from "../Features/Menu/Types";
-
-
+import {
+  TextField,
+  type FormHelperTextProps,
+  type SxProps,
+  type TextFieldVariants,
+} from "@mui/material";
+import {
+  useState,
+  type JSX,
+  type PropsWithChildren,
+  type ProviderProps,
+} from "react";
 
 interface TextFieldUI {
-    multiline? : boolean;
-    label : string;
-    variant? : TextFieldVariants;
-    error? : boolean;
-    helperText? : errorText | string;
-    Onchange : (e : any) => void
+  multiline?: boolean;
+  label: string;
+  variant?: TextFieldVariants;
+  error?: boolean;
+  helperText? : string;
+  slotProps?: {
+    input: {
+      startAdornment: JSX.Element;
+    };
+  };
+  style?: React.CSSProperties;
+  sx?: SxProps;
+  Onchange: (e: any) => void;
 }
 
-
-function TextFieldUI(children : TextFieldUI) {
-
-    return(<>
-    <TextField
-      
-     onChange={(e) => children.Onchange(e.target.value)}
-      id="standard-basic"
-      label={children.label}
-      multiline={children.multiline || false}
-      variant={children.variant}
-      error={children.error}
-      helperText={children.helperText}
-      sx={{
-      display : "flex",
-      justifyContent : "right",   
-        margin : "10px 0",
-        "& label": {
-          left: "auto",
-          right: 0,
-          transformOrigin: "right",
-        },
-        "& .MuiInput-root": {
-          textAlign: "right",
-          direction: "rtl",
-        },
-        // تحريك الخط السفلي ليبدأ من اليمين
-        "& .MuiInput-underline:after": {
-          transformOrigin: "right",
-        },
-       
-      }}
-    />
-    </>)
+function TextFieldUI(children: TextFieldUI) {
+  return (
+    <>
+      <TextField
+        onChange={(e) => children.Onchange(e.target.value)}
+        id="standard-basic"
+        label={children.label}
+        multiline={children.multiline || false}
+        variant={children.variant}
+        error={children.error}
+        helperText={children.helperText}
+        style={children.style}
+        slotProps={children.slotProps}
+        sx={children.sx}
+      />
+    </>
+  );
 }
 
-
-export default TextFieldUI
+export default TextFieldUI;
